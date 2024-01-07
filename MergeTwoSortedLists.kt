f67b48971ad1f1642154ce5615703b65a896d887
hatim enezari <hatim.enezari@gmail.com>
2024-01-07 12:00:00 +0100

    Add Merge Two Sorted Lists solution in Kotlin

---
+++ MergeTwoSortedLists.kt
@@ -0,0 +1,15 @@
+class ListNode(var : Int) {
+    var next: ListNode? = null
+}
+
+fun mergeTwoLists(list1: ListNode?, list2: ListNode?): ListNode? {
+    if (list1 == null) return list2
+    if (list2 == null) return list1
+    return if (list1. < list2.) {
+        list1.next = mergeTwoLists(list1.next, list2)
+        list1
+    } else {
+        list2.next = mergeTwoLists(list1, list2.next)
+        list2
+    }
+}
